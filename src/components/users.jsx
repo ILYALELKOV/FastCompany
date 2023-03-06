186dda6cf3764e6fa37e0171d6f1a262cd5b23d3
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-03-06 19:55:16 +1000

    create table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import api from '../api'
 
 const Users = () => {
+    const [users, setUsers] = useState(api.users.fetchAll())
+
+    const handleDeleteUser = (id) => {
+        setUsers((prevState) => prevState.filter(users => users._id !== id))
+    }
+
     return (
         <>
-            <h1>Hello</h1>
+            <span className="badge bg-primary m-3 fs-6">Тусанут с тобой</span>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map(user => (
+                    <tr key={user._id}>
+                        <td>{user.name}</td>
+                        <td>{user.qualities.map(item => (
+                            <span
+                                key={item._id}
+                                className={'m-1 badge bg-' + item.color}
+                            >
+                                {item.name}
+                            </span>
+                        ))}</td>
+                        <td>{user.profession.name}</td>
+                        <td>{user.completedMeetings}</td>
+                        <td>{user.rate + ' / 5'}</td>
+                        <td>
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => handleDeleteUser(user._id)}
+                            >
+                                delete
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     )
 }
