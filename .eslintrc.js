b4b6b01f44ab540602ff108eca0dad823430ef4f
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-03-20 15:45:02 +1000

    finished eslint setting

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ['plugin:react/recommended', 'standard'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		'comma-dangle': ['error', 'never'],
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		semi: ['error', 'never'],
+		quotes: ['error', 'single'],
+		'space-before-function-paren': ['error', 'never']
+	}
+}
