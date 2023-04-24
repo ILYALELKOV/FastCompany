e1aba03054dcca5420646b277a5d92e94af8460b
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-04-24 16:58:12 +1000

    add style in Loader component

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Loader = () => {
 	return (
-		<div className="d-flex flex-column align-items-center p-3">
+		<div className="d-flex flex-column align-items-center p-3 bg-info mt-5 rounded-4">
 			<div className="spinner-border text-warning mb-2 " role="status">
 				<span className="visually-hidden">Loading...</span>
 			</div>
-			<p className="text-dark mt-3">Подождите, идет загрузка...</p>
+			<p className="text-white fw-bold mt-3">Подождите, идет загрузка...</p>
 		</div>
 	)
 }
